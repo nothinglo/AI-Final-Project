055f83ce3dfe704a85feefd06165a8b3ba38c16a
ro6842 <ro6842@gmail.com>
2015-06-02 01:40:23 +0800

    Create sudoku-digging.h

---
+++ Sudoku/Sudoku/sudoku-digging.h
@@ -0,0 +1,22 @@
+//
+//  sudoku-board.h
+//  AI-Final-Project
+//
+//  Created by uglyman.nothinglo on 2015/5/23.
+//  Copyright (c) 2015年 nothinglo. All rights reserved.
+//
+
+#ifndef __Sudoku__sudoku_digging__
+#define __Sudoku__sudoku_digging__
+
+#include <iostream>
+#include <vector>
+#include "globalVar.h"
+#include "sudoku-board.h"
+
+using namespace std;
+
+void generateDiggingBoard(int board[][sudokuSize]);
+bool gradientDescentDigToBoard(int board[][sudokuSize]);
+
+#endif /* defined(__Sudoku__sudoku_board__) */
